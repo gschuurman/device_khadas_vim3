8c2fde51232141c83a6920ad3af9bfc567222d09
Glenn Schuurman <glennschuurman@gmail.com>
2025-11-20 20:59:46 +0100

    Fix build config

--- snapp_car_vim3.mk
+++ snapp_car_vim3.mk
@@ -38,56 +38,54 @@ $(call inherit-product, packages/services/Car/car_product/build/car.mk)
 $(call inherit-product, frameworks/base/data/fonts/fonts.mk)
 
 PRODUCT_PACKAGES += \
-	android.hardware.automotive.vehicle@2.0-default-service \
-	android.hardware.automotive.vehicle@V3-default-service \
-	android.hardware.broadcastradio@2.0-service \
-	android.hardware.broadcastradio \
-	android.hardware.automotive.remoteaccess@V2-default-service \
-	android.hardware.automotive.ivn@V1-default-service \
-	CarConnectivityOverlay \
-	CarWifiOverlay \
-	librs_jni
+    android.hardware.broadcastradio@2.0-service \
+    android.hardware.broadcastradio \
+    android.hardware.automotive.remoteaccess@V2-default-service \
+    android.hardware.automotive.ivn@V1-default-service \
+    CarConnectivityOverlay \
+    CarWifiOverlay \
+    librs_jni
 
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml \
-	frameworks/native/data/etc/car_core_hardware.xml:system/etc/permissions/car_core_hardware.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
-	frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
-	frameworks/native/data/etc/android.hardware.screen.landscape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.screen.landscape.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-	frameworks/native/data/etc/android.hardware.type.automotive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.type.automotive.xml \
-	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
-	frameworks/native/data/etc/android.software.activities_on_secondary_displays.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.activities_on_secondary_displays.xml \
-	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.ar.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.autofocus.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.any.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
-	device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml \
+    frameworks/native/data/etc/car_core_hardware.xml:system/etc/permissions/car_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
+    frameworks/native/data/etc/android.hardware.screen.landscape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.screen.landscape.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.type.automotive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.type.automotive.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
+    frameworks/native/data/etc/android.software.activities_on_secondary_displays.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.activities_on_secondary_displays.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.ar.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.autofocus.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.any.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	android.car.drawer.unlimited=true \
-	android.car.hvac.demo=true \
-	com.android.car.radio.demo=true \
-	com.android.car.radio.demo.dual=true \
-	ro.hardware.egl=mali
+    android.car.drawer.unlimited=true \
+    android.car.hvac.demo=true \
+    com.android.car.radio.demo=true \
+    com.android.car.radio.demo.dual=true \
+    ro.hardware.egl=mali
 
 PRODUCT_COPY_FILES += \
-	packages/services/Car/car_product/init/init.bootstat.rc:root/init.bootstat.rc \
-	packages/services/Car/car_product/init/init.car.rc:root/init.car.rc
+    packages/services/Car/car_product/init/init.bootstat.rc:root/init.bootstat.rc \
+    packages/services/Car/car_product/init/init.car.rc:root/init.car.rc
 
 DEVICE_MANIFEST_FILE += \
-	device/snappautomotive/vim3/manifest.xml
+    device/snappautomotive/vim3/manifest.xml
 
 # EVS - Use the mocked EVS from the emulator builds. This can be replaced
 # with a full EVS HAL implementation to integrate with the VIM3 hardware
@@ -105,13 +103,13 @@ $(call inherit-product, device/generic/car/emulator/evs/evs.mk)
 # VIM3, but there are dongles which can provide CAN bus connectivity
 
 PRODUCT_PACKAGES += \
-	android.hardware.automotive.can@1.0-service
+    android.hardware.automotive.can@1.0-service
 PRODUCT_PACKAGES_DEBUG += \
-	canhalctrl \
-	canhaldump \
-	canhalsend \
-	android.hardware.automotive.occupant_awareness@1.0-service \
-	android.hardware.automotive.occupant_awareness@1.0-service_mock
+    canhalctrl \
+    canhaldump \
+    canhalsend \
+    android.hardware.automotive.occupant_awareness@1.0-service \
+    android.hardware.automotive.occupant_awareness@1.0-service_mock
 
 BOARD_SEPOLICY_DIRS += device/google_car/common/sepolicy
 
@@ -141,14 +139,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
         persist.rcs.supported=0 \
         persist.eab.supported=0
 
-# Occupant Awareness
-PRODUCT_PACKAGES_DEBUG += \
-	android.hardware.automotive.occupant_awareness@1.0-service \
-	android.hardware.automotive.occupant_awareness@1.0-service_mock
-
-# Sepolicy for occupant awareness system
-include packages/services/Car/car_product/occupant_awareness/OccupantAwareness.mk
-
 # --- USB PERIPHERALS ---
 PRODUCT_PACKAGES += \
     audio.usb.default \
@@ -162,17 +152,12 @@ PRODUCT_PACKAGES += \
 
 BOARD_SEPOLICY_DIRS += device/snappautomotive/vim3/sepolicy
 
-# --- JOUW VEHICLE HAL ---
+# Vehicle HAL for VIM3
 PRODUCT_PACKAGES += \
     android.hardware.automotive.vehicle@snap-service
 
 # GAPPS
 $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk )
-
-PRODUCT_PROPERTY_OVERRIDES += \
-        ro.boot.wificountrycode=00 \
-        ro.config.media_vol_default=0 \
-        log.tag.CarTrustAgentUnlockEvent=I
 
 PRODUCT_NAME := snapp_car_vim3
 PRODUCT_CHARACTERISTICS := automotive
@@ -181,4 +166,3 @@ PRODUCT_MODEL := Snapp Automotive build of Android Automotive OS for VIM3
 else
 PRODUCT_MODEL := $(SNAPP_MODEL)
 endif
-
