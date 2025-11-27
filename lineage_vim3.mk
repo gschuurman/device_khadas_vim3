bd02de117f8a660de3dd6bb7fb2068710ed79302
Glenn Schuurman <gschuurman@scamander.com>
2025-11-27 13:13:22 +0100

    Change config

--- lineage_vim3.mk
+++ lineage_vim3.mk
@@ -1,29 +1,75 @@
 TARGET_VIM3 := true
 TARGET_USE_TABLET_LAUNCHER := true
-
 BOARD_IS_AUTOMOTIVE := true
 PRODUCT_DISPLAY_DENSITY := 160
 
-# CarServiceHelperService accesses the hidden api in the system server.
-SYSTEM_OPTIMIZE_JAVA := false
+PRODUCT_DEVICE := vim3
 
-DEVICE_FRAMEWORK_MANIFEST_FILE += device/google_car/common/manifest.xml
+PRODUCT_NAME := lineage_vim3
+PRODUCT_BRAND := Khadas
+PRODUCT_MODEL := VIM3 (Snapp Edition)
+PRODUCT_MANUFACTURER := Khadas
+PRODUCT_CHARACTERISTICS := automotive
 
-# generic_system.mk sets 'PRODUCT_ENFORCE_RRO_TARGETS := *'
-# but this breaks phone_car. So undo it here.
-PRODUCT_ENFORCE_RRO_TARGETS :=
+# --- INHERITANCE ---
+# 1. LineageOS Common (Geeft je de Lineage basis)
+$(call inherit-product, vendor/lineage/config/common.mk)
+# 2. Yukawa Platform Base
+$(call inherit-product, device/amlogic/yukawa/yukawa.mk)
+# 3. Android Automotive Defaults
+$(call inherit-product, packages/services/Car/car_product/build/car.mk)
+# 4. Fonts
+$(call inherit-product, frameworks/base/data/fonts/fonts.mk)
 
-# Enable mainline checking
-PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := false
+$(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
 
-# Set Car Service RRO
-PRODUCT_PACKAGES += CarServiceOverlayPhoneCar
-GOOGLE_CAR_SERVICE_OVERLAY += CarServiceOverlayPhoneCarGoogle
+# --- A/B UPDATE PACKAGES ---
+# 'bootctrl.yukawa' bestond niet, we gebruiken de generieke of die uit yukawa.mk
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_client \
+    update_verifier \
+    checkpoint_gc \
+    SystemUpdaterSample
 
-# Additional selinux policy
-BOARD_SEPOLICY_DIRS += device/google_car/common/sepolicy
+# --- BOOT HAL (FIX) ---
+# De AIDL default service bestond niet, we vallen terug op de HIDL 1.2 die Yukawa gebruikt
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.2-service \
+    android.hardware.boot@1.2-impl-recovery
 
-# Override heap growth limit due to high display density on device
+# --- AUTOMOTIVE PACKAGES ---
+PRODUCT_PACKAGES += \
+    android.hardware.automotive.remoteaccess@V2-default-service \
+    android.hardware.automotive.ivn@V1-default-service \
+    CarConnectivityOverlay \
+    librs_jni
+    # CarWifiOverlay verwijderd (veroorzaakte error)
+
+# --- EVS (CAMERA) ---
+# De standaard Google services bestonden niet in jouw tree.
+# We voegen WEL jouw configuratie toe. De services moeten we later toevoegen 
+# zodra we weten hoe ze heten in LineageOS (vaak android.hardware.automotive.evs-aidl-default).
+PRODUCT_PACKAGES += \
+    snapp_evs_configuration
+    # EvsApp en manager-service tijdelijk uitgeschakeld om build te fixen
+
+# --- CUSTOM HAL ---
+PRODUCT_PACKAGES += \
+    android.hardware.automotive.vehicle@snap-service
+
+# --- USB & AUDIO ---
+PRODUCT_PACKAGES += \
+    audio.usb.default \
+    tinyplay \
+    tinycap \
+    tinymix \
+    android.hardware.camera.provider@2.5-external-service \
+    camera.device@3.5-external-impl \
+    camera.device@3.5-impl
+    # gnss-service.usb verwijderd (veroorzaakte error)
+
+# --- PROPERTIES ---
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.heapgrowthlimit=256m \
     ro.boot.wificountrycode=00 \
@@ -34,153 +80,37 @@ PRODUCT_PROPERTY_OVERRIDES += \
     com.android.car.radio.demo=true \
     com.android.car.radio.demo.dual=true
 
-# Exclude the testing apps
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.rcs.supported=0 \
+    persist.eab.supported=0
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.vehicle.path.pwm.duty=/sys/class/pwm/pwmchip0/pwm0/duty_cycle \
+    ro.vendor.vehicle.path.pwm.enable=/sys/class/pwm/pwmchip0/pwm0/enable \
+    ro.vendor.vehicle.path.pwm.period=/sys/class/pwm/pwmchip0/pwm0/period \
+    ro.vendor.vehicle.path.gpio.reverse=/sys/class/gpio/gpio496/value \
+    ro.automotive.evs.config_file=/vendor/etc/automotive/evs/evs_configuration_override.xml
+
+# --- OVERLAYS & POLICY ---
+PRODUCT_PACKAGE_OVERLAYS += device/khadas/vim3/overlay
+BOARD_SEPOLICY_DIRS += device/google_car/common/sepolicy
+BOARD_SEPOLICY_DIRS += device/khadas/vim3/sepolicy
+
+DEVICE_MANIFEST_FILE += device/khadas/vim3/manifest.xml
+
+# --- BUILD SETTINGS ---
+SYSTEM_OPTIMIZE_JAVA := false
 PRODUCT_IS_AUTOMOTIVE_SDK := true
-
-# --- INHERITANCE ---
-$(call inherit-product, vendor/lineage/config/common.mk)
-$(call inherit-product, device/amlogic/yukawa/yukawa.mk)
-$(call inherit-product, packages/services/Car/car_product/build/car.mk)
-$(call inherit-product, frameworks/base/data/fonts/fonts.mk)
-
-# --- A/B (SEAMLESS) UPDATE PACKAGES (VERPLICHT VOOR A16) ---
-PRODUCT_PACKAGES += \
-    android.hardware.boot-service.default \
-    bootctrl.yukawa \
-    update_engine \
-    update_engine_client \
-    update_verifier \
-    checkpoint_gc
-
-# --- AUTOMOTIVE PACKAGES ---
-PRODUCT_PACKAGES += \
-    android.hardware.automotive.remoteaccess@V2-default-service \
-    android.hardware.automotive.ivn@V1-default-service \
-    CarConnectivityOverlay \
-    CarWifiOverlay \
-    librs_jni
-
-# --- COPY FILES ---
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/aosp_excluded_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/aosp_excluded_hardware.xml \
-    frameworks/native/data/etc/car_core_hardware.xml:system/etc/permissions/car_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
-    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
-    frameworks/native/data/etc/android.hardware.broadcastradio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.broadcastradio.xml \
-    frameworks/native/data/etc/android.hardware.screen.landscape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.screen.landscape.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-    frameworks/native/data/etc/android.hardware.type.automotive.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.type.automotive.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-    frameworks/native/data/etc/android.hardware.wifi.passpoint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.passpoint.xml \
-    frameworks/native/data/etc/android.software.activities_on_secondary_displays.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.activities_on_secondary_displays.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.ar.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.autofocus.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.concurrent.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.any.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
-    device/generic/car/common/android.hardware.disable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.direct.xml \
+EXCLUDE_BUILD_RAMDUMP_UPLOADER_DEBUG_TOOL := true
 
 PRODUCT_COPY_FILES += \
     packages/services/Car/car_product/init/init.bootstat.rc:root/init.bootstat.rc \
     packages/services/Car/car_product/init/init.car.rc:root/init.car.rc
 
-DEVICE_MANIFEST_FILE += \
-    device/khadas/vim3/manifest.xml
-
-# ==========================================
-# EVS (Exterior View System) - USB CAMERA
-# ==========================================
-
-PRODUCT_PACKAGES += \
-    android.hardware.automotive.evs.manager-service
-
-
-PRODUCT_PACKAGES += \
-    android.hardware.automotive.evs-service.sample
-
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.automotive.evs.config_file=/vendor/etc/automotive/evs/evs_configuration_override.xml
-
-# 4. (Optioneel) De EVS App om te testen (zit vaak al in car.mk, maar voor de zekerheid)
-PRODUCT_PACKAGES += \
-    snapp_evs_configuration \
-    EvsApp
-
-
-# CAN bus support - We don't have a direct hardware interface on the
-# VIM3, but there are dongles which can provide CAN bus connectivity
-
-PRODUCT_PACKAGES_DEBUG += \
-    canhalctrl \
-    canhaldump \
-    canhalsend \
-    android.hardware.automotive.occupant_awareness@1.0-service \
-    android.hardware.automotive.occupant_awareness@1.0-service_mock
-
-
-
-# Audio Control
-PRODUCT_PACKAGES += \
-    android.hardware.automotive.audiocontrol-service.example
-
-# Sepolicy for occupant awareness system
-include packages/services/Car/car_product/occupant_awareness/OccupantAwareness.mk
-
-# Sepolicy for compute pipe system
-include packages/services/Car/cpp/computepipe/products/computepipe.mk
-
+# Permissions (De errors hierover zijn opgelost door de niet-bestaande services te verwijderen)
 PRODUCT_COPY_FILES += \
-    device/google_car/common/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
+    frameworks/native/data/etc/car_core_hardware.xml:system/etc/permissions/car_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.screen.landscape.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.screen.landscape.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
-# Phone car targets don't support ramdump
-EXCLUDE_BUILD_RAMDUMP_UPLOADER_DEBUG_TOOL := true
-
-# Disable RCS and EAB for phone car targets
-PRODUCT_PRODUCT_PROPERTIES += \
-        persist.rcs.supported=0 \
-        persist.eab.supported=0
-
-# --- USB PERIPHERALS ---
-PRODUCT_PACKAGES += \
-    audio.usb.default \
-    tinyplay \
-    tinycap \
-    tinymix \
-    android.hardware.gnss-service.usb \
-    android.hardware.camera.provider@2.5-external-service \
-    camera.device@3.5-external-impl \
-    camera.device@3.5-impl
-
-# Custom SEPolicy
-BOARD_SEPOLICY_DIRS += device/snappautomotive/vim3/sepolicy
-
-# --- CUSTOM HAL CONFIGURATION ---
-PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.vehicle.path.pwm.duty=/sys/class/pwm/pwmchip0/pwm0/duty_cycle \
-    ro.vendor.vehicle.path.pwm.enable=/sys/class/pwm/pwmchip0/pwm0/enable \
-    ro.vendor.vehicle.path.pwm.period=/sys/class/pwm/pwmchip0/pwm0/period \
-    ro.vendor.vehicle.path.gpio.reverse=/sys/class/gpio/gpio496/value
-
-# Vehicle HAL for VIM3
-PRODUCT_PACKAGES += \
-    android.hardware.automotive.vehicle@snap-service
-
-$(call inherit-product-if-exists, vendor/partner_gms/products/gms.mk)
-
-PRODUCT_CHARACTERISTICS := automotive
-
-PRODUCT_NAME := lineage_vim3
-PRODUCT_DEVICE := vim3
-PRODUCT_BRAND := Khadas
-PRODUCT_MODEL := VIM3 Pro
-PRODUCT_MANUFACTURER := Khadas
-
-PRODUCT_PACKAGE_OVERLAYS += device/khadas/vim3/overlay
