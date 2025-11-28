32cd223cd4adef6b7ff0f4b79e5e083ebbc785c0
Glenn Schuurman <glennschuurman@gmail.com>
2025-11-28 06:09:27 +0000

    Make build target 64 bit only

--- lineage_vim3.mk
+++ lineage_vim3.mk
@@ -1,6 +1,6 @@
 
 # Inherit some common AOSP stuff
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
