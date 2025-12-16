7fc9cc84c97ec0f8ff812664e8aff48477e30b3c
Glenn Schuurman <glennschuurman@gmail.com>
2025-12-16 22:02:38 +0000

    Crashing on Car Init

--- hal/audio/device_vendor.mk
+++ hal/audio/device_vendor.mk
@@ -20,14 +20,18 @@ PRODUCT_COPY_FILES += \
     device/khadas/vim3/hal/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
     device/khadas/vim3/hal/audio/primary_audio_policy_configuration_hdmi_only.xml:$(TARGET_COPY_OUT_VENDOR)/etc/primary_audio_policy_configuration.xml \
     device/khadas/vim3/hal/audio/mixer_controls.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_controls.xml \
-    device/khadas/vim3/hal/audio/usb_audio_policy_configuration_vim3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration_vim3.xml \
-    device/khadas/vim3/hal/audio/car_audio_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/car_audio_configuration.xml \
     hardware/amlogic/yukawa/audio/audio_effects_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects_config.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
+    frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
+    frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml 
+
+
+PRODUCT_COPY_FILES += \
+    device/khadas/vim3/hal/audio/car_audio_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/car_audio_configuration.xml \
+    device/khadas/vim3/hal/audio/usb_audio_policy_configuration_vim3.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration_vim3.xml \
+
 
 # Include BayLibre Audio HAL properties
 -include device/khadas/vim3/hal/audio/audio_properties.mk
-
